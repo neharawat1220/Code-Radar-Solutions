00bfe79007547298aa84b177cd68a6a57418dd1f
neharawat1220 <neharawat1220@gmail.com>
2025-02-23 13:07:50 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int grades;
+    scanf("%d",&grades);
+    if(A){
+        printf("Excellent");
+    }else if(B){
+        printf("Good");
+    }else if(C){
+        printf("Average");
+    }else if(D){
+        printf("Below Average");
+    }else{
+        printf("Fail");
+    }
+    return 0;
+}
