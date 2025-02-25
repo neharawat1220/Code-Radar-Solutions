3daaa0bb1441b90d8ce1679726ec83f80564e147
neharawat1220 <neharawat1220@gmail.com>
2025-02-25 20:41:19 +0530

    Solution to Traffic Light Simulation

--- Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     char color,R,G,Y;
-    scanf("%c %c %c %c",&color,&R,&G,&Y);
+    scanf("%c %c %c %c", &color ,&R ,&G ,&Y);
     if((color =='R') &&(color =='r')){
         printf("Stop");
     }else if((color =='G')&&(color =='g')){
